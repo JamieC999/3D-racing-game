49428a7041d99b20e090e53c85af99b88eb38555
JamieC999 <149535033+JamieC999@users.noreply.github.com>
2024-05-21 18:16:12 -0400

    Add files via upload
    Adding up to date files

---
+++ CarController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentbreakForce;
+    private bool isBreaking;
+
+    // Settings
+    [SerializeField] private float motorForce = 1000f;
+    [SerializeField] private float brakeForce = 2000f;
+    [SerializeField] private float maxSteerAngle = 30f;
+
+    // Wheel Colliders
+    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+
+    // Wheels
+    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+
+    private void FixedUpdate() 
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput() 
+    {
+        // Steering Input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Acceleration Input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Breaking Input
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor() 
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        currentbreakForce = isBreaking ? brakeForce : 0f;
+        ApplyBreaking();
+    }
+
+    private void ApplyBreaking() 
+    {
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
+    }
+
+    private void HandleSteering() 
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels() 
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform) 
+    {
+        Vector3 pos;
+        Quaternion rot; 
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
---
+++ dde.cs
@@ -0,0 +1,91 @@
+int numberofCarbrands = 10;
+Console.WriteLine(numberofCarbrands);
+
+float averageuserSpeed = 235.5f;
+Console.WriteLine(averageuserSpeed);
+
+float averageuserSpeedarcademode = 500.6f;
+Console.WriteLine(averageuserSpeedarcademode);
+
+float averagenpcSpeed = 20.2f;
+Console.WriteLine(averagenpcSpeed);
+
+float averagenpcpoliceSpeed = 195.9;
+Console.WriteLine(averagenpcpoliceSpeed);
+
+float averageturbolowBoost = 5.0;
+Console.WriteLine(averageturbolowBoost);
+
+float averageturbomidBoost = 25.0;
+Console.WriteLine(averageturbomidBoost)
+
+float averageturbohighBoost = 100.0
+Console.WriteLine(averageturbohighBoost);
+
+bool engineFailure = true;
+Console.WriteLine(engineFailure);
+
+bool turboFailure = true;
+Console.WriteLine(turboFailure);
+
+bool superchargerFailure = true;
+Console.WriteLine(superchargerFailure);
+
+bool suspensionFailure = true;
+Console.WriteLine(suspensionFailure);
+
+bool nossystemFailed = true;
+Console.WriteLine(nossystemFailed);
+
+
+string stockColor = "white"
+string carName = "BMW"; 
+string carName = "Toyota";
+string carName = "Ford";
+string carName = "Lamborghini";
+string carName = "Dodge";
+string carName = "Honda";
+string carName = "Pagani"
+string carName = "Porsche"
+string carName = "Nissan"
+string carName = "Mazda"
+
+
+
+if (turboFailure) 
+{
+    Console.WriteLine("Turbo had a problem, come back to the garage to repair it.")
+}
+bool turboFailure = "false"
+bool turboWorking = "true"
+
+
+
+if (superchargerFailure)
+{
+    Console.WriteLine("Supercharger blew the engine.")
+}
+bool 
+
+
+if (nossystemFailed)
+{
+    Console.WriteLine("Nos system blew the engine.")
+}
+bool nossystemFailed = "true"
+bool nossystemReady = "true"
+
+if (policebustedYou)
+{
+    Console.WriteLine("The police has busted you, you have 2 strikes left of your car!!.")
+}
+bool policebustedYou = "false"
+bool escapedfromPolice = "true"
+
+public int playermaxwantedlevel = 20;
+public int playerlowwantedlevel = 1;
+public int playerstaringmoney = 0;
+public int playermaxmoney = 99,000,000;
+
+private string highheatNote = "Heads up your heat level is high, cops will be more aggresive to bust you!!!"
+
